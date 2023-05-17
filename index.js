e3b7ae3a9a1d4e76ec7f16d8bb592997ecef2a06
mastermous <133895680+mastermous@users.noreply.github.com>
2023-05-17 16:27:25 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -2,39 +2,40 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-let etudiants = [];
-app.get("/etudiants", (req, res) => {
-  res.json(etudiants);
+let etudiantArray = [];
+app.get("/etudiant", (req, res) => {
+  res.status(200).json(etudiantArray);
 });
 
-app.post("/etudiants", (req, res) => {
-  const etudiant = req.body;
-  etudiants.push(etudiant);
-  res.status(201).json(etudiant);
+app.post("/etudiant", (req, res) => {
+  etudiantArray.push(req.body);
+  res.status(201).json(req.body);
 });
 
-app.put("/etudiants/:id", (req, res) => {
+app.put("/etudiant/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const nouveauEtudiant = req.body;
-
-  let etudiant = etudiants.find((i) => i.id === id);
+  let etudiant = etudiantArray.find((i) => i.id === id);
   if (etudiant) {
     etudiant.prenom = nouveauEtudiant.prenom;
     etudiant.nom = nouveauEtudiant.nom;
     etudiant.age = nouveauEtudiant.age;
-    res.json(etudiant);
+    res.status(200).json(etudiant);
   } else {
-    res.status(404).json({ message: "étudiant non trouvé" });
+    res.status(404).json({ message: "non trouvé" });
   }
 });
 
-app.delete("/etudiants/:id", (req, res) => {
+app.delete("/etudiant/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  etudiants = etudiants.filter((i) => i.id !== id);
-  res.status(200).json({ message: "étudiant supprimé avec sucés" });
+  const etudiant = etudiantArray.find((i) => i.id === id);
+
+  if (etudiant) {
+    etudiantArray = etudiantArray.filter((i) => i.id !== id);
+    res.status(200).json({ message: "étudiant supprimé avec succès" });
+  } else {
+    res.status(404).json({ message: "non trouvé" });
+  }
 });
 
-const PORT = 8000;
-app.listen(PORT, () =>
-  console.log(`l'application a démmarré avec le port ${PORT}`)
-);
+app.listen(8000), () => console.log("Server running on port 8000");
